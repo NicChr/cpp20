ed5ba21bf8d362a0b6aa8601b576540a70125ec3
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-03-26 08:08:08 +0000

    Relocated more header inclusions

--- inst/include/cpp20/r_setup.h
+++ inst/include/cpp20/r_setup.h
@@ -6,8 +6,6 @@
 #endif
 
 #include <cpp11/sexp.hpp> // For cpp11::sexp
-#include <type_traits> // For concepts
-#include <ankerl/unordered_dense.h> // Hash maps for group IDs + unique + match
 #include <cstdint> // For uint32_t and similar
 #include <cstring> // For strcmp
 #include <string> // For C++ strings
