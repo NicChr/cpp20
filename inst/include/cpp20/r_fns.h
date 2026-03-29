ac49446e0f1d9f7bafc8eaa2b579b7294618b461
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-03-29 13:51:37 +0100

    Using arg forwarding consistently

--- inst/include/cpp20/r_fns.h
+++ inst/include/cpp20/r_fns.h
@@ -25,23 +25,23 @@ inline r_sexp find_pkg_fun(const char *name, const char *pkg, bool all_fns){
 }
 
 template<typename... Args>
-inline r_sexp eval_fn(const r_sexp& r_fn, const r_sexp& envir, Args... args){
+inline r_sexp eval_fn(const r_sexp& r_fn, const r_sexp& envir, Args&&... args){
   // Expression
-  r_sexp expr = internal::make_call(r_fn, args...);
+  r_sexp expr = internal::make_call(r_fn, std::forward<Args>(args)...);
   // Evaluate expression
   return eval(expr, envir);
 }
 
 template<typename... Args>
-inline r_sexp eval_fn(const r_sym& r_fn, const r_sexp& envir, Args... args){
+inline r_sexp eval_fn(const r_sym& r_fn, const r_sexp& envir, Args&&... args){
   // Expression
-  r_sexp expr = internal::make_call(r_fn, args...);
+  r_sexp expr = internal::make_call(r_fn, std::forward<Args>(args)...);
   // Evaluate expression
   return eval(expr, envir);
 }
 
 template<typename... Args>
-inline r_sexp eval_fn(const r_str& r_fn, const r_sexp& envir, Args... args){
+inline r_sexp eval_fn(const r_str& r_fn, const r_sexp& envir, Args&&... args){
   return eval_fn(r_sym(r_fn), envir, std::forward<Args>(args)...);
 }
 
