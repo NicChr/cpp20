cc9c9612eec8b7340170bcc845e39bebc8931d1c
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-05-15 08:53:06 +0100

    New helper unlisted_length

--- inst/include/cppally/r_list_helpers.h
+++ inst/include/cppally/r_list_helpers.h
@@ -33,6 +33,22 @@ inline void recycle_impl(r_vec<r_sexp>& x, r_size_t common_size) {
 
 }
 
+inline r_size_t unlisted_length(const r_vec<r_sexp>& x){
+    safe[R_CheckStack](); // Check C Stack size isn't close to the limit
+    r_size_t n = x.length();
+    r_size_t out = 0;
+    for (r_size_t i = 0; i < n; ++i){
+        out += view_sexp(x.view(i), [&]<typename T>(const T& elem) -> r_size_t {
+            if constexpr (is<T, r_vec<r_sexp>>){
+                return unlisted_length(elem);
+            } else {
+                return length(elem);
+            }
+        });
+    }
+    return out;
+  }
+
 }
 
 #endif
